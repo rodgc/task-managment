4208cee22e3e9121a902e5bb6c82384fb15fb1fe
rodgc <rod.gc@hotmail.com>
2020-06-04 13:00:31 -0500

    Add Deployment config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -6,6 +6,10 @@ import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
@@ -13,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'topSecret51',
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
     });
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('Bootstrap');
-  const port = 3000;
+  const port = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 
